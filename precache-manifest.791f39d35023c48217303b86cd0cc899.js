a3314e238ccb8a5d8b5e1f123bf2f488be302318
moofoo <nathancookdev@gmail.com>
2019-09-30 07:07:27 -0500

    Updates

---
+++ precache-manifest.791f39d35023c48217303b86cd0cc899.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef5bdfcb73ead2fff4b2c0780f17817",
+    "url": "/hierarchical-task-list/index.html"
+  },
+  {
+    "revision": "d4c81ffb15e558894639",
+    "url": "/hierarchical-task-list/static/css/main.fee16f9f.chunk.css"
+  },
+  {
+    "revision": "d1635aed0770626fa36c",
+    "url": "/hierarchical-task-list/static/js/2.3f07414a.chunk.js"
+  },
+  {
+    "revision": "d4c81ffb15e558894639",
+    "url": "/hierarchical-task-list/static/js/main.15d5dd51.chunk.js"
+  },
+  {
+    "revision": "2f92e292d407971d2b09",
+    "url": "/hierarchical-task-list/static/js/runtime~main.c7943657.js"
+  }
+]);
